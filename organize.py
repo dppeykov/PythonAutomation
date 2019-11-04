60a34e9a9ee36859578b80fdbc42bce10c751ee8
Damyan Peykov <dppeykov@gmail.com>
2019-11-04 09:46:22 +0100

    Update organize.py

--- organize.py
+++ organize.py
@@ -18,16 +18,17 @@ def pick_directory(value):
 
 
 def organize_directory():
-    for item in os.scandir():
-        if item.is_dir():
+    # scandir will return an object with all the contents of the current directory, where the script is executing
+    for item in os.scandir(): 
+        if item.is_dir(): # if it is a directory - we are skipping it
             continue
-        file_path = Path(item)
-        file_type = file_path.suffix.lower()
-        directory = pick_directory(file_type)
+        file_path = Path(item) # gets the name of the files in the directory - name.suffix
+        file_type = file_path.suffix.lower()  # returns .txt, .pdf ...
+        directory = pick_directory(file_type) # uses the pick directory function to choose the right dir for the file
         directory_path = Path(directory)
-        if not directory_path.is_dir():
+        if not directory_path.is_dir(): # if the directory doen't exist - it will create it with mkdir()
             directory_path.mkdir()
-        file_path.rename(directory_path.joinpath(file_path))
+        file_path.rename(directory_path.joinpath(file_path)) # will move the file to the appropriate folder
 
 
 organize_directory()
