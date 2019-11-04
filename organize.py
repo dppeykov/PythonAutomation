1c398fe80059a2f47b8e3553a00eff8a8251fbda
Damyan Peykov <dppeykov@gmail.com>
2019-11-04 09:32:37 +0100

    Organizing files
    The script organizes files in the same directory by their type - moves the files to the corresponding folder

---
+++ organize.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+
+SUBDIRECTORIES = {
+    "DOCUMENTS": ['.pdf', '.rtf', '.txt'],
+    "AUDIO": ['.m4a', '.m4b', '.mp3'],
+    "VIDEO": ['.mov', '.avi', '.mp4'],
+    "IMAGES": ['.jpg', '.jpeg', '.png'],
+}
+
+
+def pick_directory(value):
+    for category, suffixes in SUBDIRECTORIES.items():
+        for suffix in suffixes:
+            if suffix == value:
+                return category
+    return "MISC"
+
+
+def organize_directory():
+    for item in os.scandir():
+        if item.is_dir():
+            continue
+        file_path = Path(item)
+        file_type = file_path.suffix.lower()
+        directory = pick_directory(file_type)
+        directory_path = Path(directory)
+        if not directory_path.is_dir():
+            directory_path.mkdir()
+        file_path.rename(directory_path.joinpath(file_path))
+
+
+organize_directory()
